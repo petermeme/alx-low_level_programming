8ade568b34a7f6f45fa828fe80027c428764a13e
petermeme <petermeme90@gmail.com>
2023-08-07 14:53:06 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,11 @@
 #include "main.h"
-/*
- * main -function that reads text file and prints to the POSIX standard output
+
+/**
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
  *
- * return: 0
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
