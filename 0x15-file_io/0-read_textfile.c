7f4737fc5a0c7a4d221dd51fa24be059f10983a0
petermeme <petermeme90@gmail.com>
2023-08-07 14:38:42 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/*
+ * main - a function that reads a text file and prints it to the POSIX standard output
+ *
+ * return: 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *letter;
+	ssize_t file, b ,c;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	letter = malloc(letters);
+	if (letter == NULL)
+	{
+		return(0);
+	}
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		return (0);
+	}
+
+	b = read(file, letter, letters);
+	if (b == -1)
+	{
+		free(letter);
+		return (0);
+	}
+	c = write(STDOUT_FILENO, letter, b);
+	if (c < 0)
+	{
+		return (0);
+	}
+	close(file);
+
+	return (c);
+}
