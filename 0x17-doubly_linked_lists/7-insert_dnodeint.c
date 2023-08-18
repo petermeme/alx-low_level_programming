9450c80e9fca3717221a2a7ff426b0f4702d9d92
petermeme <petermeme90@gmail.com>
2023-08-18 13:35:13 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,11 +29,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_nod->n = n;
+	new_node->n = n;
 	new_node->next = res_nod->next;
 	new_node->prev = res_nod;
 	res_nod->next->prev = new_node;
 	res_nod->next = new_node;
 
-	return (new_mem);
+	return (new_node);
 }
